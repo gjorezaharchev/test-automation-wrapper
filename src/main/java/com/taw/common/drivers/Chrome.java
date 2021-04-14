808097f14801c51f1766cd5f389f89181efa8282
gjore.zaharchev <>
2021-04-14 20:44:15 +0200

    added experimental capabilities

--- src/main/java/com/taw/common/drivers/Chrome.java
+++ src/main/java/com/taw/common/drivers/Chrome.java
@@ -3,21 +3,18 @@ package com.taw.common.drivers;
 import com.taw.common.Global;
 import com.taw.common.utility.Constants;
 import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
 
 /**
- *
  * @author gjore.zaharchev
- *
  */
 public class Chrome extends Global implements Drivers {
 
@@ -32,7 +29,7 @@ public class Chrome extends Global implements Drivers {
             System.out.println("Your OS is not support!!");
         }
 
-        if (System.getProperty("remote","false").equalsIgnoreCase("true")){
+        if (System.getProperty("remote", "false").equalsIgnoreCase("true")) {
             /*ChromeOptions chromeOptions = new ChromeOptions();
             //chromeOptions.setCapability("browserName", "chrome");
             //chromeOptions.setCapability("version", "89.0");
@@ -43,11 +40,11 @@ public class Chrome extends Global implements Drivers {
                 chromeOptions.addArguments("--headless");
             }*/
             try {
-                driver = new RemoteWebDriver(new URL(nodeURL),options());
+                driver = new RemoteWebDriver(new URL(nodeURL), options());
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
-        }else {
+        } else {
             driver = new ChromeDriver(options());
         }
 
@@ -61,7 +58,7 @@ public class Chrome extends Global implements Drivers {
         chromeOptions.addArguments("--allow-running-insecure-content");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--enable-automation");
-        String headless = System.getProperty("headless","false");
+        String headless = System.getProperty("headless", "false");
         String headlessGlobals = Constants.getGlobalProperty("headless");
         if (headless.equalsIgnoreCase("true") || headlessGlobals.equalsIgnoreCase("true")) {
             chromeOptions.addArguments("--headless");
@@ -69,6 +66,26 @@ public class Chrome extends Global implements Drivers {
 
         chromeOptions.addArguments("--disable-gpu");
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        if (System.getProperty("project", "").equalsIgnoreCase("sidel")) {
+            String pathToDownload = System.getProperty("user.home") + Constants.getGlobalProperty("download.location");
+            HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+            chromePrefs.put("plugins.always_open_pdf_externally", Boolean.valueOf(Constants.getGlobalProperty("plugins.always_open_pdf_externally")));
+            chromePrefs.put("download.prompt_for_download", Boolean.valueOf(Constants.getGlobalProperty("download.prompt_for_download")));
+            chromePrefs.put("profile.default_content_settings.popups", Constants.getGlobalProperty("profile.default_content_settings.popups"));
+            chromePrefs.put("download.default_directory", pathToDownload);
+            chromePrefs.put("browser.set_download_behavior", "{ behavior: 'allow', downloadPath: '" + pathToDownload + "'}");
+            chromeOptions.setExperimentalOption("prefs", chromePrefs);
+
+            chromeOptions.setCapability("browserName", Constants.getGlobalProperty("browserName"));
+            chromeOptions.setCapability("version", Constants.getGlobalProperty("version"));
+            chromeOptions.setCapability("platform", Constants.getGlobalProperty("platform"));
+            chromeOptions.setCapability("enableVNC", Boolean.valueOf(Constants.getGlobalProperty("enableVNC")));
+            chromeOptions.setCapability("enableVideo", Boolean.valueOf(Constants.getGlobalProperty("enableVNC")));
+            //chromeOptions.setCapability("screenResolution", "1920x1080x24");
+
+        }
+
         return chromeOptions;
     }
 
